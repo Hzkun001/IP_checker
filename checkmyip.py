378c67da890913a8c433bbc949d7553155948592
KomporHitam <90337549+Hzkun001@users.noreply.github.com>
2022-10-10 08:51:08 +0800

    Add files via upload
    Tools by Hzkun~

---
+++ checkmyip.py
@@ -0,0 +1,27 @@
+import socket
+import requests
+
+def print_logo(self):
+        colors = [36, 32, 34, 35, 31, 37]
+
+        x = """
+
+
+                    Black HaT Hackers
+ ██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗    ██╗██████╗
+██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝    ██║██╔══██╗
+██║     ███████║█████╗  ██║     █████╔╝     ██║██████╔╝
+██║     ██╔══██║██╔══╝  ██║     ██╔═██╗     ██║██╔═══╝
+╚██████╗██║  ██║███████╗╚██████╗██║  ██╗    ██║██║
+ ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝    ╚═╝╚═╝
+                    Hz-exploit
+
+     Note! : We don't Accept any responsibility for any illegal usage.     >
+    """
+print('{x}')
+print('Tools by Hz_Exploit')
+hostname = input('Enter a domain name: ')
+ip_address = socket.gethostbyname(hostname)
+
+print(f'Domain Name: {hostname}')
+print(f'IP Address: {ip_address}')
